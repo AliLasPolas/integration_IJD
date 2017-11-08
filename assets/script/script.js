57b8826dc684f9396741719a951b898dc37e768e
ali <ali.nizamuddin@lepoles.com>
2017-11-08 10:58:04 +0100

    ajout responsive

---
+++ assets/script/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let scroll = 0;
+
+let height = $('header>nav').attr('height');
+
+$(function(){
+	console.info('tout roule chef');
+	$(window).on('scroll', function(e) {
+		if (scroll < 110) {
+			if ( $(document).scrollTop() > 110 ) {
+				$('.navplaceholder').show();
+				$('header>nav').css('position', 'fixed');
+				$('header>nav').css('top', height);
+				$('header>nav').css('padding-top', '0');
+				$('header>nav').css('padding-bottom', '0');
+				$('header>nav').css('background-color', 'white');
+				$('header>nav').css('box-shadow', '0px 4px 2px -2px rgba(192,192,192,0.3)');
+				$('header>nav').velocity({
+					top: '0px'
+				}, 300);
+			}
+		}
+		else{
+			if($(document).scrollTop() < 110){
+				$('.navplaceholder').hide();
+				$('header>nav').css('position', 'relative');
+				$('header>nav').css('top', '0px');
+				$('header>nav').css('background-color', 'transparent');
+				$('header>nav').css('box-shadow', 'unset');
+			}
+		}
+		scroll = $(document).scrollTop();
+	});
+
+});
